2759df3e8513d7cfdfa9cbcb100f582538e6ba53
hoedlralph <ic17b003@technikum-wien.at>
2018-05-21 21:15:48 +0200

    modified project task2

--- tasks/task2/task2/Program.cs
+++ tasks/task2/task2/Program.cs
@@ -5,7 +5,23 @@ using System.Text;
 
 namespace task2
 {
-    public class Notebook
+
+    public interface IAsset
+    {
+        /// <summary>
+        /// Gets a textual description of this item.
+        /// </summary>
+        string Description { get; }
+
+        /// <summary>
+        /// Gets the item's price in the specified currency.
+        /// </summary>
+        string Asset_number(decimal numb);
+    }
+
+
+
+    public class Notebook : IAsset
     {
         private decimal m_price;
         private decimal main_memory;
@@ -79,9 +95,106 @@ namespace task2
 
             }
         }
+
+
+        #region IAsset implementation
+
+        public string Description => "Notebook";
+
+        public string Asset_number(decimal numb)
+        {
+            if (numb < 10000) throw new Exception("Inventarnummer zu niedrig");
+
+            string help = "";
+            help = System.Convert.ToString(numb);
+            help += "N";
+            return help;
+        }
+
+        #endregion
     }
 
+    public class Server : IAsset
+    {
+        private decimal m_price;
+        private decimal cores;
+        private decimal main_memory;
+        private string os;
+        private string database;
+        private decimal memory;
 
+
+        public Server(decimal m_price, decimal cores ,decimal main_memory, string os, string database,decimal memory)
+        {
+
+            if (string.IsNullOrWhiteSpace(os)) throw new ArgumentException("keine Grafikkarte vorhanden", nameof(os));
+            if (cores % 2 != 0) throw new Exception("ungerade Anzahl an Cores");
+            
+            M_price = m_price;
+            this.main_memory = main_memory;
+            this.os = os;
+            this.database = database;
+            this.cores = cores;
+            this.memory = memory;
+        }
+
+
+        public decimal M_price
+        {
+
+
+
+            get { return m_price; }
+            set
+            {
+                if (value < 0)
+                {
+                    throw new Exception("Preis zu niedrig");
+                }
+                m_price = value;
+
+
+            }
+
+
+        }
+
+
+        public decimal Main_memory
+        {
+
+            get { return main_memory; }
+
+        }
+
+
+        public string Os { get { return os; } set { os = value; } }
+
+        public decimal Memory { get { return memory; } set { memory = value; } }
+
+
+        public void main_mem(decimal mem)
+        {
+            if (mem < 64000) throw new Exception("zu wenig Hauptspeicher");
+            main_memory = mem;
+        }
+
+        #region IAsset implementation
+
+        public string Description => "Server";
+
+        public string Asset_number(decimal numb)
+        {
+            if (numb < 10000) throw new Exception("Inventarnummer zu niedrig");
+
+            string help = "";
+            help = System.Convert.ToString(numb);
+            help += "S";
+            return help;
+        }
+
+        #endregion
+    }
 
     class Program
     {
@@ -95,11 +208,25 @@ namespace task2
                      asus.M_price = 200;
                      Console.WriteLine(asus.M_price);
 
-            }catch (Exception e)
+                Server mic = new Server(100000, 8, 128000, "Microsoft", "Oracle", 256000);
+                Server lin = new Server(50000, 4, 64000, "Linux", "DB2", 512000);
+                var list1 = new List<IAsset> { asus, mic, lin };
+
+                decimal i = 0;
+                foreach (IAsset s in list1)
+                {
+                    Console.WriteLine(s.Description);
+                    Console.WriteLine(s.Asset_number(12000+i));
+                    i++;
+                }
+            }
+            catch (Exception e)
             {
 
                 Console.WriteLine(e.Message);
             }
+
+            
 
 
         }
